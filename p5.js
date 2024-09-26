cd2f62f4c3a0fc2e1be0a5db0ae2c6a1974520fe
outplatinum <vinicius.damasio@escola.pr.gov.br>
2024-09-26 13:39:15 -0300

    Create p5.js

---
+++ p5.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background("white");
+}
+
+function draw() {
+  stroke("red")
+  fill("black");
+  if(mouseIsPressed){
+  rect(mouseX,mouseY,20,20);  
+  }
+
+}
